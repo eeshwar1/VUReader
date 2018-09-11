23bd5d4e32b61e6e9dbeb75a2397458a85c55e08
Venky Venkatakrishnan <eeshwar1@gmail.com>
2018-09-10 20:24:02 -0500

    Initial Commit

---
+++ VUReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VUReader
+//
+//  Created by Venky Venkatakrishnan on 9/10/18.
+//  Copyright © 2018 Venky UL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
